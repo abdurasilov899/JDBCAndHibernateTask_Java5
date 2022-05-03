97fd9f86d11f313734aeb0f9c691a5c760edc08e
User <abdurasilov99@gmail.com>
2022-05-03 19:00:22 +0600

    Task8

--- src/main/java/peaksoft/dao/UserDaoHibernateImpl.java
+++ src/main/java/peaksoft/dao/UserDaoHibernateImpl.java
@@ -1,5 +1,8 @@
 package peaksoft.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Query;
 import peaksoft.model.User;
 import peaksoft.util.Util;
 
@@ -7,38 +10,76 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoHibernateImpl implements UserDao {
+public class UserDaoHibernateImpl implements UserDao,AutoCloseable {
 
-    public UserDaoHibernateImpl() throws SQLException {
+
+    private final EntityManagerFactory entityManagerFactory = Util.createEntityManagerFactory();
+
+    public UserDaoHibernateImpl() {
 
     }
 
     @Override
     public void createUsersTable() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        System.out.println("GOOD JOB...!!!");
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+
     }
 
     @Override
     public void dropUsersTable() {
-
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        Query query = entityManager.createNativeQuery("DROP TABLE USERS");
+        query.executeUpdate();
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        User user = new User(name, lastName, age);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     @Override
     public List<User> getAllUsers() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<User> listUser = entityManager.createNativeQuery("Select a from User a  ", User.class)
+                .getResultList();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        return listUser;
+    }
 
-    return  null;}
 
     @Override
     public void cleanUsersTable() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.createNativeQuery("TRUNCATE table users").executeUpdate();
+        entityManager.getTransaction().commit();
+        entityManager.close();
 
     }
 
@@ -47,6 +88,21 @@ public class UserDaoHibernateImpl implements UserDao {
         // eger databasede parametrine kelgen firstnamege okshosh adam bar bolso
         // anda true kaitarsyn
         // jok bolso anda false kaitarsyn.
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+            Query query = entityManager.createNativeQuery("select case when count(u) > 0 " +
+                    "then true " +
+                    "else false end " +
+                    "from User u where u.name = ?1", Boolean.class);
+            query.setParameter(1, firstName);
+            Boolean singleResult = (Boolean) query.getSingleResult();
+            entityManager.getTransaction().commit();
+            entityManager.close();
+            return singleResult;
+    }
 
-    return false;}
+    @Override
+    public void close() throws Exception {
+
+    }
 }
--- src/main/java/peaksoft/util/Util.java
+++ src/main/java/peaksoft/util/Util.java
@@ -1,12 +1,37 @@
 package peaksoft.util;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import peaksoft.model.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
     private final Connection connection;
+    public static EntityManagerFactory createEntityManagerFactory() {
+        Properties properties = new Properties();
+        properties.setProperty(Environment.DRIVER, "org.postgresql.Driver");
+        properties.setProperty(Environment.URL, "jdbc:postgresql://localhost:5432/postgres");
+        properties.setProperty(Environment.USER, "postgres");
+        properties.setProperty(Environment.PASS, "1234");
+        properties.setProperty(Environment.HBM2DDL_AUTO, "update");
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty(Environment.SHOW_SQL, "true");
+
+        Configuration configuration = new Configuration();
+
+        configuration.setProperties(properties);
+
+        configuration.addAnnotatedClass(User.class);
+
+        return configuration.buildSessionFactory().unwrap(EntityManagerFactory.class);
+    }
 
     public Util() throws SQLException {
         connection = DriverManager.getConnection(
