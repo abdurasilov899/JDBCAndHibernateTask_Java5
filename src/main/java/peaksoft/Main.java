460227190996bcdceefe27f808c1cf0129c69298
User <abdurasilov99@gmail.com>
2022-05-03 22:42:56 +0600

    Task-8

--- src/main/java/peaksoft/Main.java
+++ src/main/java/peaksoft/Main.java
@@ -11,15 +11,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
-//        Util util = new Util();
-//        System.out.println(util.getConnection());
 
-        UserDao user = new UserDaoJdbcImpl();
-        //user.createUsersTable();
-//        user.saveUser("Tologon","Ibraev", (byte) 30);
-        // user.removeUserById(1);
-        //System.out.println(user.getAllUsers());
-        System.out.println(user.existsByFirstName("Tologon"));
     }
 }
 
